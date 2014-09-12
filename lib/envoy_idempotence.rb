90a0a5c604feafe417d74f61c38f202174b3816d
Lee Henson <lee.m.henson@gmail.com>
2014-09-12 14:18:54 +0100

    only load the publisher worker if celluloid is loaded

--- lib/envoy_idempotence.rb
+++ lib/envoy_idempotence.rb
@@ -1,6 +1,6 @@
 require 'docket'
 require 'envoy_idempotence/publisher'
-require 'envoy_idempotence/publisher_worker'
+require 'envoy_idempotence/publisher_worker' if defined?(::Celluloid)
 require 'envoy_idempotence/middleware'
 
 module EnvoyIdempotence
